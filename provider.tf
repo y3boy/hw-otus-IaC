372e1e5f90a0edb00ee7634a33d0d29f1e6848ba
Anushervon Nabiev <nabievanush1@gmail.com>
2022-01-23 12:18:04 +0300

    Done hw #2

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "yandex" {
+    token     = var.yc_token
+    cloud_id  = var.yc_cloud
+    folder_id = var.yc_folder
+}
+
+terraform {
+    required_providers {
+        yandex = {
+            source = "yandex-cloud/yandex"
+        }
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "yc_cloud" {
+    type = string
+    description = "Yandex Cloud ID"
+}
+
+variable "yc_folder" {
+    type = string
+    description = "Yandex Cloud folder"
+}
+
+variable "yc_token" {
+    type = string
+    description = "Yandex Cloud token "
+}
+
+variable "db_password" {
+    type = string
+    description = "MySQL password"
+}
